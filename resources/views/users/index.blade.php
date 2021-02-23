7921341e851e9d7c1ef81763c39cb34862e2cc96
mazc0de <dandroid36@gmail.com>
2021-02-23 19:35:33 +0700

    adding page user management

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h4>
+            ok
+        </h4>
+    </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>breadcumbs</h4>
+                        <div class="card-header-action">
+                            <a href="{{ route('users.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Add User</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        @if (session('success'))
+                        <div class="alert alert-success alert-dismissible show fade">
+                            <div class="alert-body">
+                                <button class="close" data-dismiss="alert">
+                                    <span>×</span>
+                                </button>
+                                {{session('success')}}
+                            </div>
+                            </div>
+                        @endif
+                        @if (session('delete'))
+                        <div class="alert alert-danger alert-dismissible show fade">
+                            <div class="alert-body">
+                                <button class="close" data-dismiss="alert">
+                                    <span>×</span>
+                                </button>
+                                {{session('delete')}}
+                            </div>
+                            </div>
+                        @endif
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Roles</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <th scope="row">{{ $loop->iteration }}</th>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->username }}</td>
+                                        <td>
+                                            @if (!empty($user->getRoleNames()))
+                                                @foreach($user->getRoleNames() as $roles)
+                                                    <label class="badge badge-success"><i class="fas fa-user-lock"></i> {{ $roles }}</label>
+                                                @endforeach
+                                            @endif
+                                        </td>
+                                        <td>
+                                            {{-- <a href="{{ route('users.show',$user)}}" class="btn btn-primary" >show</a> --}}
+                                            {{-- @role('super-admin') --}}
+                                                <a href="{{ route('users.edit',$user)}}" class="btn btn-primary" >edit</a>
+                                                <a href="{{route('users.delete',$user)}}" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this item?');">Delete</a>
+                                            {{-- @endrole --}}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
