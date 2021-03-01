62f3fbef96c3590e1f56519dc41fdb1c2e572e78
mazc0de <dandroid36@gmail.com>
2021-03-01 16:08:18 +0700

    update views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,17 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
+                            <div class="col-12 mb-4">
+                                <div class="hero text-white hero-bg-image hero-bg-parallax" style="background-image: url({{asset('img/profile.png')}});">
+                                  <div class="hero-inner">
+                                    <h2>Welcome back!</h2>
+                                    <p class="lead">You're logged as</p>
+                                    <div class="mt-4">
+                                      <a href="#" class="btn btn-outline-white btn-lg btn-icon icon-left"><i class="far fa-user"></i> Setup Account</a>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             <h3 class="text-center">Dashboard Content</h3>
                         </div>
                     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <title>{{ $title}}</title>
+    <title>{{ $title}} | CV. Surya Utama</title>
     <link rel="icon" href="{{asset('img/logo-cv.png')}}" type="image/x-icon" width='100px'>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 4.1.1 -->
