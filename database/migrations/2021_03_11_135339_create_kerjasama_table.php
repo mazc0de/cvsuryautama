7ed348501c556986fed6a0e742ff5fc06c596596
mazc0de <dandroid36@gmail.com>
2021-03-13 22:47:04 +0700

    migration for kerjasama table

---
+++ database/migrations/2021_03_11_135339_create_kerjasama_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKerjasamaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kerjasama', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nama_perusahaan');
+            $table->string('hp');
+            $table->string('luas_lahan');
+            $table->string('alamat');
+            $table->string('keterangan');
+            $table->string('photo');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kerjasama');
+    }
+}
