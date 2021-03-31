85bba411ac0c2872cb647e0c58533969c82bbe33
mazc0de <dandroid36@gmail.com>
2021-03-31 20:17:51 +0700

    adding smtp mail

---
+++ resources/views/home/contact/contact-form.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Thank you for your message
+<strong>Name : </strong> {{$data['name']}}
+<br>
+<strong>Email : </strong> {{$data['email']}}
+<br>
+<strong>Message : </strong> {{$data['message']}}
+@endcomponent
