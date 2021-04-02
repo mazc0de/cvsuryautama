a12d70822c23cba17babbcc8582639d5c801d02e
mazc0de <dandroid36@gmail.com>
2021-04-02 21:01:40 +0700

    moving function store

--- app/Http/Controllers/KerjasamaController.php
+++ app/Http/Controllers/KerjasamaController.php
@@ -40,45 +40,7 @@ class KerjasamaController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        // dd($request->all());
-        $this->validate($request, [
-            'nama' => 'required',
-            'instansi' => 'required',
-            'email' => 'required|email',
-            'alamat' => 'required',
-            'no_hp' => 'required|numeric',
-            'luas_lahan' => 'required|numeric',
-            'photos' => 'required',
-            'photos.*' => 'mimes:png,jpg,jpeg',
-        ]);
-        // str_replace(' ', '-', $request->namaperusahaan)
-        if($request->hasFile('photos')){
-            foreach($request->file('photos') as $file){
-                $name = str_replace(' ', '-', $request->nama_perusahaan) .'-' . time() . '-' . $file->getClientOriginalName();
-                $file->move(public_path().'/kerjasama/', $name);
-                $data[] = $name;
-            }
-        }
-
-        Kerjasama::create([
-            'nama' => $request->nama,
-            'instansi' => $request->instansi,
-            'email' => $request->email,
-            'alamat' => $request->alamat,
-            'hp' => $request->no_hp,
-            'luas_lahan' => $request->luas_lahan,
-            'keterangan' => $request->keterangan,
-            'photo' => json_encode($data)
-        ]);
-        // $item = new Kerjasama();
-        // $item->photo = json_encode($data);
-        // $item->save();
-
-        return back()->with('success', 'Terima kasih! tim kami akan menghubungi anda lebih lanjut');
-
-    }
+   
 
     /**
      * Display the specified resource.
