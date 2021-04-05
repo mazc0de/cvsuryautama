d2e7da34478c0099a41984b68bbb429fed3b11ae
mazc0de <dandroid36@gmail.com>
2021-04-05 19:45:03 +0700

    change username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -19,6 +20,21 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // public function authenticate(Request $request){
+    //     // Inputan yg diambil
+    //     $credentials = $request->only('username', 'password');
+
+    //     if (Auth::attempt($credentials)) {
+    //         // Jika berhasil login
+
+    //         return redirect()->route('dashboard');
+
+    //         //return redirect()->intended('/details');
+    //     }
+    //     // Jika Gagal
+    //     return redirect('login');
+    // }
 
     public function logout(){
         Auth::logout();
